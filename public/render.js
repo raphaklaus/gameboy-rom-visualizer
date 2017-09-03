ad772faddbdfa63d51b200e7988e017dbcc0343a
raphaklaus <raphaklaus@gmail.com>
2017-09-03 01:19:43 -0300

    feat: add grid

--- public/render.js
+++ public/render.js
@@ -1,4 +1,6 @@
-const context = document.getElementById('render').getContext('2d');
+const canvasTiles = document.getElementById('canvas-tiles').getContext('2d'),
+  canvasGrid = document.getElementById('canvas-grid').getContext('2d')
+
 var pixelSize = 1,
   rowIndex = 0,
   colIndex = 0,
@@ -8,13 +10,17 @@ var pixelSize = 1,
   verticalPos = 1,
   horizontalPos = 1,
   offset = 0,
+  mouseX, mouseY,
+  previousMouseX, previousMouseY,
   scrollVelocity = 1;
+
+canvasTiles.scale(canvasZoom, canvasZoom);
+canvasGrid.scale(canvasZoom, canvasZoom);
 
 function load(){
   var file = document.getElementById('file').files[0];
   var reader = new FileReader();
   reader.onload = function (file) {
-    context.scale(canvasZoom, canvasZoom);
     data = changePalette(file.target.result.split(',').map(item => +item));
     render(data, 0);
     
@@ -42,6 +48,29 @@ function toggleScroll() {
     scrollVelocity = 1;
 }
 
+function drawSquare(event) {
+  // todo: enhance variable names mouseX and mouseY, their meaning is: squareX and squareY
+  previousMouseX = mouseX;
+  previousMouseY = mouseY;
+  mouseX = Math.trunc(event.offsetX / (8 * 6));
+  mouseY = Math.trunc(event.offsetY / (8 * 6));
+
+  console.log('x', mouseX);
+  console.log('prevx', previousMouseX);
+  
+  if (previousMouseX !== mouseX || previousMouseY !== mouseY) {
+    canvasGrid.clearRect(0, 0, 480, 480);
+    canvasGrid.beginPath();
+    canvasGrid.lineWidth = '1';
+    canvasGrid.strokeStyle = '#e2cc24';
+    canvasGrid.rect(mouseX * 8, mouseY * 8, 8, 8);
+    canvasGrid.stroke();
+
+    document.querySelectorAll('.address-indicator > strong')[0].innerHTML = 
+      '#' + Number((mouseX + ((offset / 64) + mouseY * 10)) * 16).toString('16');
+  }
+}
+
 function changePalette(data) {
   console.log(data[0])
   return data.map(item => {
@@ -64,7 +93,6 @@ function render(data, offset) {
   horizontalPos = 0;
 
   data.slice(offset, 6400 + offset).map((item, index) => {
-
     if (index % 64 === 0) {
       colIndex = 0;
       rowIndex = 0;
@@ -76,13 +104,13 @@ function render(data, offset) {
       }
 
       data.slice(offset, 6400 + offset).slice(index, 64 * (tile + 1)).map((item, index) => {
-        context.fillStyle = item;
+        canvasTiles.fillStyle = item;
         if (index > 0 && index % 8 === 0) {
           colIndex++;
           rowIndex = 0;
         }
 
-        context.fillRect(8 * horizontalPos * pixelSize + rowIndex * pixelSize, 
+        canvasTiles.fillRect(8 * horizontalPos * pixelSize + rowIndex * pixelSize, 
           8 * verticalPos * pixelSize + colIndex * pixelSize, pixelSize, pixelSize); 
         rowIndex++;
       });
