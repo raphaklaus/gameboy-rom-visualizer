92f490674176b33dfcb6d146b8a164f489231b74
Raphael Klaus <raphaklaus@gmail.com>
2018-05-24 11:25:31 -0300

    chore: add distribution

--- dist/parser.js
+++ dist/parser.js
@@ -1,13 +1,13 @@
 "use strict";
 function parser(file, byteLength) {
     const binary = new Uint8Array(file, 0, byteLength);
-    let result = [];
+    const result = [];
     for (let offset = 0; offset < byteLength; offset += 2) {
-        let highByte = binary[offset];
-        let lowByte = binary[offset + 1];
+        const highByte = binary[offset];
+        const lowByte = binary[offset + 1];
         for (let bit = 0; bit < 8; bit++) {
-            let mostSignificantBit = (highByte << bit) & 0x80 ? (1 << 1) : 0;
-            let leastSignificantBit = (lowByte << bit) & 0x80 ? 1 : 0;
+            const mostSignificantBit = (highByte << bit) & 0x80 ? (1 << 1) : 0;
+            const leastSignificantBit = (lowByte << bit) & 0x80 ? 1 : 0;
             result.push(mostSignificantBit | leastSignificantBit);
         }
     }
--- dist/render.js
+++ dist/render.js
@@ -4,8 +4,8 @@ const canvasGrid = document.getElementById('canvas-grid');
 if (!canvasTiles || !canvasGrid)
     throw new Error('No canvas found');
 const contextTiles = canvasTiles.getContext('2d'), contextGrid = canvasGrid.getContext('2d');
-const pixelSize = 1, canvasZoom = 6;
-let data, mouseX, mouseY, previousMouseX, previousMouseY, rowIndex = 0, tile = 0, colIndex = 0, offset = 0, scrollVelocity = 1, verticalPos = 1, file, horizontalPos = 1;
+const pixelSize = 1, tileWidth = 8, tileHeight = 8, canvasZoom = 6;
+let mouseX, mouseY, previousMouseX, previousMouseY, offset = 0, scrollVelocity = 1, file;
 contextTiles.scale(canvasZoom, canvasZoom);
 contextGrid.scale(canvasZoom, canvasZoom);
 const load = () => {
@@ -16,26 +16,22 @@ const load = () => {
     const reader = new FileReader();
     reader.onload = (event) => {
         file = parser(reader.result, reader.result.byteLength);
-        data = changePalette(file);
-        render(data, 0);
-        const onScroll = (event) => {
-            if (event.wheelDeltaY < 0)
-                offset += 640 * scrollVelocity;
+        render(changePalette(file));
+        const onScroll = (scrollEvent) => {
+            if (scrollEvent.wheelDeltaY < 0)
+                offset += scrollVelocity * 640;
             else
-                offset -= 640 * scrollVelocity;
+                offset -= scrollVelocity * 640;
             if (offset < 0)
                 offset = 0;
-            render(data, offset);
+            render(changePalette(file, offset), offset);
         };
         window.onwheel = onScroll;
     };
     reader.readAsArrayBuffer(incomingFile);
 };
 const toggleScroll = () => {
-    if (scrollVelocity === 1)
-        scrollVelocity = 10;
-    else
-        scrollVelocity = 1;
+    scrollVelocity = (scrollVelocity === 1) ? 10 : 1;
 };
 const drawSquare = (event) => {
     // todo: enhance variable names mouseX and mouseY, their meaning is: squareX and squareY
@@ -51,11 +47,13 @@ const drawSquare = (event) => {
         contextGrid.rect(mouseX * 8, mouseY * 8, 8, 8);
         contextGrid.stroke();
         document.querySelectorAll('.address-indicator > strong')[0].innerHTML =
-            '#' + Number((mouseX * 16 + (((offset / 64) * 16) + mouseY * 10 * 16))).toString(16);
+            `#${Number((mouseX * 16 + (((offset / 64) * 16) + mouseY * 10 * 16)))
+                .toString(16)}`;
     }
 };
-function changePalette(data) {
-    return data.map(item => {
+const changePalette = (data, offset = 0) => {
+    return data.slice(offset, offset + 6400)
+        .map(item => {
         if (item === 0)
             return '#222';
         else if (item === 1)
@@ -64,33 +62,28 @@ function changePalette(data) {
             return '#aaa';
         return '#f44620';
     });
-}
-const render = (data, offset) => {
-    rowIndex = 0;
-    colIndex = 0;
-    tile = 0;
-    verticalPos = 0;
-    horizontalPos = 0;
-    data.slice(offset, 6400 + offset).map((item, index) => {
+};
+const render = (data, offset = 0) => {
+    let colIndex = 0, rowIndex = 0, tile = 0, verticalPos = 0, horizontalPos = 0;
+    data.forEach((pixel, index) => {
         if (index % 64 === 0) {
-            colIndex = 0;
             rowIndex = 0;
+            colIndex = 0;
             if (tile % 10 === 0) {
                 horizontalPos = 0;
                 if (tile > 0)
                     verticalPos++;
             }
-            data.slice(offset, 6400 + offset).slice(index, 64 * (tile + 1)).map((item, index) => {
-                contextTiles.fillStyle = item;
-                if (index > 0 && index % 8 === 0) {
-                    colIndex++;
-                    rowIndex = 0;
-                }
-                contextTiles.fillRect(8 * horizontalPos * pixelSize + rowIndex * pixelSize, 8 * verticalPos * pixelSize + colIndex * pixelSize, pixelSize, pixelSize);
-                rowIndex++;
-            });
-            horizontalPos++;
+            else
+                horizontalPos++;
             tile++;
         }
+        else if (index % 8 === 0) {
+            rowIndex++;
+            colIndex = 0;
+        }
+        contextTiles.fillStyle = pixel;
+        contextTiles.fillRect((tileWidth * horizontalPos) + colIndex, (tileHeight * verticalPos) + rowIndex, pixelSize, pixelSize);
+        colIndex++;
     });
 };
