8d2b22fadaa56ee318f28adc97c0530dd7d2869f
Raphael Klaus <raphaklaus@gmail.com>
2019-03-24 17:38:55 +0100

    feat: add super nintendo 4bpp format

--- src/parser.ts
+++ src/parser.ts
@@ -1,4 +1,4 @@
-function parser(file: ArrayBuffer, byteLength: number): Array<number> {
+function parser2bpp(file: ArrayBuffer, byteLength: number): Array<number> {
   const binary = new Uint8Array(file, 0, byteLength)
   const result = []
 
@@ -10,6 +10,33 @@ function parser(file: ArrayBuffer, byteLength: number): Array<number> {
       const mostSignificantBit = (highByte << bit) & 0x80 ? (1 << 1) : 0
       const leastSignificantBit = (lowByte << bit) & 0x80 ? 1 : 0
       result.push(mostSignificantBit | leastSignificantBit)
+    }
+  }
+
+  return result
+}
+
+// must get 32 next bytes
+function parser4bpp(file: ArrayBuffer, byteLength: number): Array<number> {
+  const binary = new Uint8Array(file, 0, byteLength)
+  const result = []
+
+  for (let offset = 0; offset < byteLength; offset += 2) {
+    const currentBitPlane = binary[offset]
+    const secondBitPlane = binary[offset + 1]
+    const thirdBitPlane = binary[offset + 14]
+    const forthBitPlane = binary[offset + 15]
+
+    for (let bit = 0; bit < 8; bit++) {
+      const MSB = (forthBitPlane << bit) & 0x80 ? (1 << 3) : 0
+      const firstMiddleBit = (thirdBitPlane << bit) & 0x80 ? (1 << 2) : 0
+      const secondMiddleBit = (secondBitPlane << bit) & 0x80 ? (1 << 1) : 0
+      const LSB = (currentBitPlane << bit) & 0x80 ? 1 : 0
+
+      result.push(MSB | firstMiddleBit | secondMiddleBit | LSB)
+    //   const mostSignificantBit = (highByte << bit) & 0x80 ? (1 << 1) : 0
+    //   const leastSignificantBit = (lowByte << bit) & 0x80 ? 1 : 0
+    //   result.push(mostSignificantBit | leastSignificantBit)
     }
   }
 
--- src/render.ts
+++ src/render.ts
@@ -14,7 +14,7 @@ const pixelSize = 1,
 
 let mouseX: number, mouseY: number, previousMouseX, previousMouseY,
   offset = 0,
-  scrollVelocity = 1,
+  scrollVelocity = 6,
   file: Array<number>
 
 contextTiles.scale(canvasZoom, canvasZoom)
@@ -29,12 +29,12 @@ const load = () => {
   const incomingFile = fileElement.files[0] as File
 
   const reader = new FileReader()
-  reader.onload =  (event: Event) => {
-    file = parser(reader.result, reader.result.byteLength)
+  reader.onload = function(): void {
+    file = parser4bpp(this.result as ArrayBuffer, (this.result as ArrayBuffer).byteLength)
     render(changePalette(file))
 
     const onScroll = (scrollEvent: MouseWheelEvent) => {
-      if (scrollEvent.wheelDeltaY < 0)
+      if (scrollEvent.deltaY > 0)
         offset += scrollVelocity * 640
       else
         offset -= scrollVelocity * 640
@@ -72,23 +72,63 @@ const drawSquare = (event: MouseEvent) => {
 
     document.querySelectorAll('.address-indicator > strong')[0].innerHTML =
       `#${Number((mouseX * 16 + (((offset / 64) * 16) + mouseY * 10 * 16)))
-      .toString(16)}`
+        .toString(16)}`
   }
 }
 
 const changePalette = (data: Array<number>, offset = 0): Array<string> => {
   return data.slice(offset, offset + 6400)
-  .map(item => {
-    if (item === 0)
-      return '#222'
-    else if (item === 1)
-      return '#d8af81'
-    else if (item === 2)
-      return '#aaa'
+    .map(item => {
+      if (item === 0)
+        return '#000'
+      else if (item === 1)
+        return '#ffffff'
+      else if (item === 2)
+        return '#a8734a'
+      else if (item === 3)
+        return '#e9b287'
+      else if (item === 4)
+        return '#772d26'
+      else if (item === 5)
+        return '#b66862'
+      else if (item === 6)
+        return '#85d4dc'
+      else if (item === 7)
+        return '#c5ffff'
+      else if (item === 8)
+        return '#a85fb4'
+      else if (item === 9)
+        return '#e99df5'
+      else if (item === 10)
+        return '#559e4a'
+      else if (item === 11)
+        return '#92df87'
+      else if (item === 12)
+        return '#42348b'
+      else if (item === 13)
+        return '#7e70ca'
+      else if (item === 14)
+        return '#bdcc71'
+      else if (item === 15)
+        return '#ffffb0'
 
-    return '#f44620'
-  })
+      return '#f44620'
+    })
 }
+
+// const changePalette = (data: Array<number>, offset = 0): Array<string> => {
+//   return data.slice(offset, offset + 6400)
+//     .map(item => {
+//       if (item === 0)
+//         return '#222'
+//       else if (item === 1)
+//         return '#d8af81'
+//       else if (item === 2)
+//         return '#aaa'
+
+//       return '#f44620'
+//     })
+// }
 
 const render = (data: Array<string>, offset = 0) => {
   let colIndex = 0,
